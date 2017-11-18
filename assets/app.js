a7b9037632ad75dd44b3dbeec2685266d1fa8650
Fabian Piel <fabipiel@de.ibm.com>
2017-11-18 19:23:56 +0100

    Add more elements, default event handling, disable zoom

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,21 @@
 // Initialize application
 $(document).ready(function() {
-    console.log('hello');
+    var click = 0
+    $('body').click(function( event ) {
+        event.stopPropagation();
+        click++;
+        switch (click) {
+            case 1:
+                console.log('clicked 1');
+                break;
+            case 2:
+                console.log('clicked 2');
+                break;
+            default:
+                console.log('nothing planned to happen.');
+        };
+    })
+    console.log('ready.');
+
+
 });
